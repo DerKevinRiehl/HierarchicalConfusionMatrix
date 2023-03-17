9fe99624b5710b8dbc177244b1e6223317cc21b4
DerKevinRiehl <50080024+DerKevinRiehl@users.noreply.github.com>
2023-03-17 16:37:29 +0100

    Update Example_GermEval2019_Task1B.py

--- Example_GermEval2019_Task1B.py
+++ Example_GermEval2019_Task1B.py
@@ -11,7 +11,7 @@
 import os
 import numpy as np
 import networkx as nx
-from HierarchicalConfusion import determineHierarchicalConfusionMatrix, getLeafNode
+from hierarchical_confusion_matrix import determineHierarchicalConfusionMatrix, getLeafNode
 
 # Methods
 """
